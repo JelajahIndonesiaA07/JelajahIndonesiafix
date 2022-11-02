2f9ec91ca01424f13de88e250d219cba7bd4e45f
Soraya Sabrina <94454821+sorayasab@users.noreply.github.com>
2022-11-02 16:11:12 +0700

    Update urls.py

--- tempat_wisata/urls.py
+++ tempat_wisata/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 
 from tempat_wisata.views import allData, index
-# from tempat_wisata.viewset_api import *
 from rest_framework import routers
 
 urlpatterns = [
