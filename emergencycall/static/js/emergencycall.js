a144c1de8e16223422cd2aece0a08110168f18b4
Savero Arkabuana <savero.arkabauana@gmail.com>
2022-11-02 15:04:33 +0700

    test

--- emergencycall/static/js/emergencycall.js
+++ emergencycall/static/js/emergencycall.js
@@ -56,3 +56,14 @@ $(document).ready(function () {
             getHospital();
         })
     }
+
+    
+    $(document).ready(function () {
+        $('.navbar-light .dmenu').hover(function () {
+                $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+            }, function () {
+                $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+            });
+        });
+    
+  
