c574ae561a288f619bdc944d5305f120e8fcfc76
irsyadmufid <irsyadm90@gmail.com>
2022-12-06 15:20:38 +0700

    add views for post test 1

--- activity/urls.py
+++ activity/urls.py
@@ -10,6 +10,7 @@ from activity.views import AddActivity
 from activity.views import show_json
 from activity.views import hapus
 from activity.views import show_activity_json
+from activity.views import AddActivity_flutter
 
 app_name = 'activity'
 
@@ -25,4 +26,5 @@ urlpatterns = [
     path("json/", show_json, name="show_json"),
     path("hapus/<int:id>", hapus, name="hapus"),
     path('get-activity-flutter/', show_activity_json, name="show_activity_json"),
+    path("add-activity-flutter/", AddActivity_flutter, name="AddActivity_flutter"),
 ]
