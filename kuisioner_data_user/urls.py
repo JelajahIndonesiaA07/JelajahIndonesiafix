a0092033c6073182f590b1396e8b03762cb18157
ayyubieied <ayyubieied@gmail.com>
2022-12-06 21:31:38 +0700

    FEAT "Revisi views"

--- kuisioner_data_user/urls.py
+++ kuisioner_data_user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, hasil
+from .views import get_json, index, hasil
 
 app_name = "kuisioner_data_user"
 urlpatterns = [
     path('', index, name='index'),
-    path('hasil', hasil, name='hasil')
+    path('hasil', hasil, name='hasil'),
+    path('json', get_json, name='json')
 ]
