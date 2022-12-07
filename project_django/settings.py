5f753302c0ff01452cbd2dd3c98a2c4d17035dab
irsyadmufid <irsyadm90@gmail.com>
2022-12-07 16:57:45 +0700

    settinhs

--- project_django/settings.py
+++ project_django/settings.py
@@ -30,7 +30,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [f'https://jelajah-indonesia.up.railway.app']
-
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
+CORS_ALLOWED_ORIGINS = ['https://jelajah-indonesia.up.railway.app',]
+CORS_ORIGIN_WHITELIST = ('https://jelajah-indonesia.up.railway.app',)
 # Application definition
 
 INSTALLED_APPS = [
@@ -53,6 +60,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,7 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'project_django.urls'
@@ -149,12 +157,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SAMESITE = 'None'
-SESSION_COOKIE_SAMESITE = 'None'
-
-CORS_ALLOWED_ORIGINS = ['https://jelajah-indonesia.up.railway.app',]
-CORS_ORIGIN_WHITELIST = ('https://jelajah-indonesia.up.railway.app',)
