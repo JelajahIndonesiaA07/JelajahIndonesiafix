cdfd0b0ce091aa19e02fad54f43278911f703fd3
Soraya Sabrina <sorayasabrina@Sorayas-MacBook-Air.local>
2022-12-09 21:04:47 +0700

    views flutter

--- tempat_wisata/urls.py
+++ tempat_wisata/urls.py
@@ -4,6 +4,8 @@ from tempat_wisata.views import add_tempat_wisata
 from tempat_wisata.views import get_tempat_wisata
 from tempat_wisata.views import delete_tempat_wisata
 from tempat_wisata.views import show_tempat_wisata_json
+from tempat_wisata.views import flutter_add_tempat_wisata
+from tempat_wisata.views import flutter_delete_tempat_wisata
 
 app_name = 'tempat_wisata'
 
@@ -13,4 +15,6 @@ urlpatterns = [
     path('get-tempat-wisata/', get_tempat_wisata, name='get_tempat_wisata'),
     path('delete-tempat-wisata/<int:id>', delete_tempat_wisata, name='delete_tempat_wisata'),
     path('get-tempat-wisata-flutter/', show_tempat_wisata_json, name="show_tempat_wisata_json"),
+    path('add-tempat-wisata-flutter/', flutter_add_tempat_wisata, name="flutter_add_tempat_wisata"),
+    path('delete-tempat-wisata-flutter/', flutter_delete_tempat_wisata, name="flutter_delete_tempat_wisata"),
 ]
--- tempat_wisata/views.py
+++ tempat_wisata/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.core import serializers
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.http import JsonResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from tempat_wisata.models import tempat_wisata_Item
@@ -49,3 +50,22 @@ def delete_tempat_wisata(request, id):
 def show_tempat_wisata_json(request):
     data = tempat_wisata_Item.objects.all()
     return HttpResponse(serializers.serialize("json", data),content_type="application/json")
+
+# flutter
+@csrf_protect
+def flutter_add_tempat_wisata(request):
+    if request.method == "POST":
+        form = WisataForm(request.POST)
+        form.instance.user = request.user
+        if form.is_valid():
+            form.save()
+            return JsonResponse({"message": "Successfully Add Tempat Wisata!.","status": 200}, status=200)
+    else:
+        return JsonResponse({"message": "Failed to Add Tempat Wisata!.","status": 401}, status=401)
+    return JsonResponse({"message": "Failed to Add Tempat Wisata!.", "status": 401}, status=401)
+
+requires_csrf_token
+def flutter_delete_tempat_wisata(request, id):
+    task = tempat_wisata_Item.objects.get(id=id)
+    task.delete()
+    return JsonResponse({"message": "Successfully Delete Tempat Wisata!.", "status": 200}, status=200)
